766e8e6c57d893118b7b61c81ffd76fb2d8c2060
Hp <Hp@MERVESU>
2024-08-26 14:46:45 +0300

    Add project files.

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+            int number = 7;
+            Console.WriteLine(number + " is " + CheckEvenOrOdd(number));
+        }
+            public static string CheckEvenOrOdd(int number)
+            {
+                if (number % 2 == 0)
+                    return "Even";
+                else
+                    return "Odd";
+            }
+
+
+    }
+}
+
